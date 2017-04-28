f28ba5a0c2163c94125aa2ce11caab7b85761065
ccsexyz <mofaxyz@gmail.com>
2017-04-28 15:15:25 +0800

    refine Mux

--- mux.go
+++ mux.go
@@ -56,7 +56,7 @@ func (p *packet) marshal(b []byte) {
 }
 
 type Mux struct {
-	conn      net.Conn // underlying connection, must be reliable
+	net.Conn  // underlying connection, must be reliable
 	lock      sync.Mutex
 	wlock     sync.Mutex
 	writech   chan packet
@@ -72,7 +72,7 @@ type Mux struct {
 
 func NewMux(conn net.Conn) (mux *Mux, err error) {
 	mux = &Mux{
-		conn:     conn,
+		Conn:     conn,
 		die:      make(chan bool),
 		writech:  make(chan packet),
 		acceptch: make(chan *MuxConn),
@@ -125,7 +125,7 @@ func (mux *Mux) Close() (err error) {
 		return
 	}
 	close(mux.die)
-	err = mux.conn.Close()
+	err = mux.Conn.Close()
 	var conns []*MuxConn
 	mux.connsLock.Lock()
 	for _, v := range mux.conns {
@@ -139,7 +139,7 @@ func (mux *Mux) Close() (err error) {
 }
 
 func (mux *Mux) Addr() net.Addr {
-	return mux.conn.LocalAddr()
+	return mux.LocalAddr()
 }
 
 func (mux *Mux) getID() (id uint16, err error) {
@@ -220,7 +220,7 @@ func (m *Mux) writePacket(pkt packet) (err error) {
 
 func (mux *Mux) readPacket() (pkt packet, err error) {
 	pktbuf := make([]byte, pktHdrlen)
-	_, err = io.ReadFull(mux.conn, pktbuf)
+	_, err = io.ReadFull(mux, pktbuf)
 	if err != nil {
 		return
 	}
@@ -240,7 +240,7 @@ func (mux *Mux) readPacket() (pkt packet, err error) {
 			} else {
 				pktlen -= len(buf)
 			}
-			_, err = io.ReadFull(mux.conn, buf)
+			_, err = io.ReadFull(mux, buf)
 			if err != nil {
 				return
 			}
@@ -249,7 +249,7 @@ func (mux *Mux) readPacket() (pkt packet, err error) {
 	default:
 		if pktlen > 0 {
 			buf := make([]byte, pktlen)
-			_, err = io.ReadFull(mux.conn, buf)
+			_, err = io.ReadFull(mux, buf)
 		}
 	}
 	return
@@ -323,12 +323,12 @@ func (mux *Mux) writeLoop() {
 			pkt.payload = zerobuf[:rand.Intn(16)]
 		}
 		pkt.marshal(pktbuf)
-		_, err := mux.conn.Write(pktbuf)
+		_, err := mux.Write(pktbuf)
 		if err != nil {
 			return
 		}
 		if len(pkt.payload) != 0 {
-			_, err = mux.conn.Write(pkt.payload)
+			_, err = mux.Write(pkt.payload)
 			if err != nil {
 				return
 			}
@@ -598,14 +598,14 @@ func (addr *Addr) String() string {
 
 func (conn *MuxConn) LocalAddr() (addr net.Addr) {
 	return &Addr{
-		Addr: conn.mux.conn.LocalAddr(),
+		Addr: conn.mux.LocalAddr(),
 		id:   conn.id,
 	}
 }
 
 func (conn *MuxConn) RemoteAddr() (addr net.Addr) {
 	return &Addr{
-		Addr: conn.mux.conn.RemoteAddr(),
+		Addr: conn.mux.RemoteAddr(),
 		id:   conn.id,
 	}
 }
